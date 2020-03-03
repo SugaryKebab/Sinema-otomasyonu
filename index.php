ff6c7cf010a89e62149a28aabc584d6ac92aa273
samed <naturalfusen2@gmail.com>
2020-03-03 19:34:53 +0300

    commit

---
+++ index.php
@@ -0,0 +1,258 @@
+
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/style.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <script src="https://kit.fontawesome.com/de1126d062.js" crossorigin="anonymous"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+  <div class="bar">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-end">
+      <a class="navbar-brand" href="#"> Sinema</a>
+    
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Ana Sayfa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Vizyondaki Filmler</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Gelecek Filmler</a>
+          </li>
+          <li class="nav-item ">
+              <a class="nav-link" href="#">İletişim</a>
+            </li>
+        </ul>
+      </div>
+     
+      <div class="dropdown">
+        <button type="button " class="btn btn-success dropdown-toggle" data-toggle="dropdown">
+          <i class="far fa-user"> Kullanıcı</i>
+        </button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Link 1</a>
+          <a class="dropdown-item" href="#">Link 2</a>
+          <a class="dropdown-item" href="#">Link 3</a>
+        </div>
+      </div>
+
+  </nav>
+  
+
+  </div>
+  
+
+
+  <div id="demo" class="carousel slide" data-ride="carousel">
+
+    <!-- Indicators -->
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+      <li data-target="#demo" data-slide-to="2"></li>
+    </ul>
+  
+    <!-- The slideshow -->
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="img-fluid" src="images/slider/1.jpg" alt="garez">
+      </div>
+      <div class="carousel-item">
+        <img class="img-fluid" src="images/slider/2.jpg" alt="vahseitn">
+      </div>
+      <div class="carousel-item">
+        <img src="ny.jpg" alt="New York">
+      </div>
+    </div>
+  
+    <!-- Left and right controls -->
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+  
+  </div>
+
+
+   <div class="filmler" style="margin-top: 50px;" >
+    <div class="container">
+        <div class="buton">
+            <div class="btn-group-lg" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-link">Vizyonda</button>
+                <button type="button" class="btn btn-link">Yakında</button>
+              </div>
+
+        </div>
+   
+
+
+
+        <div style="margin-top: 50px;"  id="carousel-example" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner row w-100 mx-auto" role="listbox">
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3 active">
+                
+                  <div class="img1-wrap">
+                    <img src="images/film/parazit.jpg" alt="Avatar" class="image">
+                    <div class="overlay">
+                      <div class="text">Bilet Al</div>
+                    </div>
+                  </div>
+            
+                </div>
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+
+
+                  <div class="img1-wrap">
+                    <img src="images/film/sonic.png" alt="Avatar" class="image">
+                    <div class="overlay">
+                      <div class="text"><a href="#"> Bilet Al</a></div>
+                    </div>
+                  </div>
+
+                </div>
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                  
+                  <div class="img1-wrap">
+                    <img src="images/film/kucuk.png" alt="Avatar" class="image">
+                    <div class="overlay">
+                      <div class="text"><a href="#"> Bilet Al</a></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                  
+                  <div class="img1-wrap">
+                    <img src="images/film/joker.jpg" alt="Avatar" class="image">
+                    <div class="overlay">
+                      <div class="text"><a href="#"> Bilet Al</a></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                    <img src="images/film/parazit.jpg" class="img-fluid mx-auto d-block" alt="img5">
+                </div>
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                    <img src="images/film/parazit.jpg" class="img-fluid mx-auto d-block" alt="img6">
+                </div>
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                    <img src="images/film/parazit.jpg" class="img-fluid mx-auto d-block" alt="img7">
+                </div>
+                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                    <img src="images/film/parazit.jpg" class="img-fluid mx-auto d-block" alt="img8">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carousel-example" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel-example" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+   </div>
+
+
+
+
+
+
+
+
+
+
+
+
+   <div id="giris" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+  
+      <!-- Modal content-->
+      <div class="modal-content" style="padding: 15px;">
+        <form action="/action_page.php">
+          <div class="form-group">
+            <label for="email">E-posta:</label>
+            <input type="email" class="form-control" placeholder="E-posta adresiniz" id="email">
+          </div>
+          <div class="form-group">
+            <label for="pwd">Şifre:</label>
+            <input type="password" class="form-control" placeholder="Şifre" id="pwd">
+          </div>
+        
+          <button type="submit" class="btn btn-primary">Giriş Yap</button>
+        </form>
+
+
+
+
+
+
+
+      </div>
+  
+    </div>
+  </div>
+
+
+
+
+    <script>
+        $('#carousel-example').on('slide.bs.carousel', function (e) {
+    /*
+        CC 2.0 License Iatek LLC 2018 - Attribution required
+    */
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 5;
+    var totalItems = $('.filmler .carousel-item').length;
+ 
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $(' .filmler .carousel-item').eq(i).appendTo('.filmler .carousel-inner');
+            }
+            else {
+                $('.filmler .carousel-item').eq(0).appendTo('.filmler .carousel-inner');
+            }
+        }
+    }
+});
+    </script>
+
+
+
+</body>
+
+
+
+
+<footer>
+
+
+</footer>
+
+</html>
