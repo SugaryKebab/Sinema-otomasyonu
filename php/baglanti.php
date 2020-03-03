8905dd853658f052f5a8ae9b4c0bf55d5bcacf01
samed <naturalfusen2@gmail.com>
2020-03-03 19:29:13 +0300

    commit

---
+++ php/baglanti.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=sinema", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
